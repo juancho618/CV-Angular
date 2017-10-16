9bfb37a3b276b99307872a6b8411930c0731d5b9
juan <jjsorianoe@gmail.com>
2017-10-16 12:26:58 +0200

    initial changes in the portfolio projects

---
+++ static/js/app/controllers/portfolioController.js
@@ -0,0 +1,26 @@
+app.controller('portfolioCtrl', function()  {
+    let self = this;
+    self.projects = [
+        {
+            title : 'Smart Charging Stations',
+            description : 'a description of the option',
+            image : ''
+        },
+        {
+            title : 'anoother',
+            description : 'some other des',
+            image : ''
+        },
+        {
+            title : 'anoother',
+            description : 'some other des',
+            image : ''
+        },
+        {
+            title : 'anoother',
+            description : 'some other des',
+            image : ''
+        }
+    ];
+
+});
