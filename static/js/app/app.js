2f398070c53c2336fa98fc1719dbf83af7705716
juan <jjsorianoe@gmail.com>
2017-10-17 21:02:48 +0200

    Adding animate

--- static/js/app/app.js
+++ static/js/app/app.js
@@ -1,1 +1,1 @@
-let app = angular.module("cvAPP", [ 'ngMaterial', 'ngAnimate' ]);
+let app = angular.module("cvAPP", [ 'ngMaterial' ]);
--- static/js/app/controllers/envelopeController.js
+++ static/js/app/controllers/envelopeController.js
@@ -1,10 +1,12 @@
 app.controller('envelopeCtrl', function($window){
     let self = this;
     self.isOpen = false;
+    self.dialog = false;
 
     self.start = () => {
         let env = angular.element( document.querySelector('#envelope'));
         env.addClass("animated bounceOutLeft");
+        self.dialog = true;
     }
     
 
